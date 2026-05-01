0f01a6d7d2f90b24b1596f6a43a1c2f22882d5e0
Samuel .A <samuelashrafmakram1998@gmail.com>
2026-05-01 20:37:52 +0300

    test

--- handlers.go
+++ handlers.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func customHandler(w http.ResponseWriter, r *http.Request) {
+func customeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
